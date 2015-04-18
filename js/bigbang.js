a5380d9296cc74f50c200515378482ed34f844cc
Étienne Després <etienne@pochour.com>
2015-04-18 13:33:04 -0400

    On s'approche de quelque chose.

--- js/bigbang.js
+++ js/bigbang.js
@@ -2,7 +2,7 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight, 0.1, 50);
 camera.position.z = 10;
 
-var renderer = new THREE.WebGLRenderer({ antialias: true });
+var renderer = new THREE.CanvasRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -10,16 +10,16 @@ var loader = new THREE.JSONLoader(); // init the loader util
 var mesh;
 
 loader.load('js/model.js', function (geometry) {
-  var material = new THREE.MeshBasicMaterial({
-    wireframe: true,
-    color: 0xEB3A0A
-  });
+  var material = new THREE.MeshBasicMaterial({ color: 'black'});
+  mesh = new THREE.Mesh(geometry, material);
   
-  mesh = new THREE.Mesh(
-    geometry,
-    material
-  );
-
+  mesh.rotation.x = 0.3;
+  mesh.visible = false;
+  
+  var egh = new THREE.EdgesHelper(mesh, 0xEB3A0A, 0.01);
+  egh.material.linewidth = 0.7;
+  scene.add(egh);
+  
   scene.add(mesh);
 });
 
@@ -27,6 +27,9 @@ var render = function() {
 	requestAnimationFrame(render);
 	var time = Date.now() * 0.001;
 	renderer.render(scene, camera);
+  if (typeof mesh !== 'undefined') {
+    mesh.rotation.y += 0.001;
+  }
 };
 
 window.addEventListener('resize', function () {
